0f9991b7a37338f3be40f8cc1800786f561cd322
Omar Trejo <otrenav@gmail.com>
2016-05-31 01:09:41 -0500

    URL building mechanism

--- functions/shared/connections.R
+++ functions/shared/connections.R
@@ -13,8 +13,7 @@ if (!require("RCurl")) {
     require(RCurl)
 }
 
-api_get <- function(request, query="") {
-    URL <- paste(base_URL, request, format_string, query, sep = "")
+api_get <- function(URL) {
     print_info("Getting data...", URL)
     response <- NULL
     tryCatch({
@@ -25,4 +24,47 @@ api_get <- function(request, query="") {
     })
     print_success("Data retrieved", URL)
     return(response)
+}
+
+
+build_URL <- function(parameters, loop=NULL) {
+    ##
+    ## NOTES:
+    ## 1. `max_results` is retrieved from the configuration
+    ## 2. `page` and `limit` depend no DRF configuration
+    ##
+    update <- update_string(parameters)
+    URL_pre <- paste(base_URL, parameters$path, sep="")
+    ## TODO: Take into account the
+    ##`query` case for pagination
+    if (!is.null(loop)) {
+        URL_mid <- paste(update, "/?limit=", max_results, "&page=", loop, "&", sep="")
+    } else if (!is.null(parameters$id)) {
+        URL_mid <- paste("/", parameters$id, update, "/?", sep="")
+    } else {
+        URL_mid <- paste(update, "/?", sep="")
+    }
+    URL <- paste(URL_pre, URL_mid, format_string, sep="")
+    return(URL)
+}
+
+build_URL_for_graph <- function(parameters) {
+    if (!is.null(parameters$update) && parameters$update == "y") {
+        print_warning("Overriding `update` with `show_graph`")
+    }
+    URL_pre <- paste(base_URL, parameters$path, sep="")
+    URL_mid <- paste("/", parameters$id, "/graph/", sep="")
+    URL <- paste(URL_pre, URL_mid, sep="")
+    return(URL)
+}
+
+
+update_string <- function(parameters) {
+    update <- NULL
+    if (!is.null(parameters$update) && parameters$update == "y") {
+        update <- "/update"
+    } else {
+        update <- ""
+    }
+    return(update)
 }
