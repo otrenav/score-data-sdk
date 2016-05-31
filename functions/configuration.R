7a7c15bbbd3961e80a200b4149a396f0e0767d00
Omar Trejo <otrenav@gmail.com>
2016-05-31 01:05:57 -0500

    Add a maximum number of results in configuration

--- functions/configuration.R
+++ functions/configuration.R
@@ -5,6 +5,7 @@
 ##
 ## status_level = [ALL | ERRORS | DEBUG]
 ##
-base_URL = "https://testing-dot-score-group.appspot.com"
-format_string = "/?format=json"
-status_level = "DEBUG"
+base_URL      <- "https://testing-dot-score-group.appspot.com"
+format_string <- "format=json"
+status_level  <- "DEBUG"
+max_results   <- 10000
