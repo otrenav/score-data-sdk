180e6fec0dede9dd1c11d956ab7e4d7cbb7067f8
Omar Trejo <otrenav@gmail.com>
2016-05-31 01:10:16 -0500

    Override option for print messages and warning

--- functions/shared/prints.R
+++ functions/shared/prints.R
@@ -4,35 +4,47 @@
 ## Datata, 2016
 ##
 
-print_success <- function(message, detail="") {
+print_success <- function(message, detail="", override=NULL) {
     if (status_level != "ERRORS") {
         next_message <- paste("[+] SUCCESS: ", message, sep="")
-        print_status(next_message, detail)
+        print_status(next_message, detail, override)
     }
 }
 
-print_info <- function(message, detail="") {
+
+print_info <- function(message, detail="", override=NULL) {
     if (status_level != "ERRORS") {
         next_message <- paste("[ ] INFO: ", message, sep="")
-        print_status(next_message, detail)
+        print_status(next_message, detail, override)
     }
 }
 
-print_error <- function(message, detail="") {
+
+print_error <- function(message, detail="", override=NULL) {
     next_message <- paste("[!] ERROR: ", message, sep="")
-    print_status(next_message, detail)
+    print_status(next_message, detail, override)
     stop()
 }
 
-print_status <- function(message, detail="") {
+
+print_warning <- function(message, detail="", override=NULL) {
+    if (status_level != "ERRORS") {
+        next_message <- paste("[!] WARNING: ", message, sep="")
+        print_status(next_message, detail, override)
+    }
+}
+
+
+print_status <- function(message, detail="", override=NULL) {
     next_message <- paste("\n", message, sep="")
-    if (status_level == "DEBUG") {
+    if (status_level == "DEBUG" || (!is.null(override) & override)) {
         next_message <- paste(message, " (", detail, ")\n", sep="")
     } else {
         next_message <- paste(message, "\n", sep="")
     }
     print_raw(next_message)
 }
+
 
 print_raw <- function(message) {
     cat(sprintf(message))
